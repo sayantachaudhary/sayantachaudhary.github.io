0afa7408106535948c619fcc18b406bc58aef8cb
Sayanta Chaudhary <sayanta01@gmail.com>
2026-03-30 03:28:38 +0530

    more

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ function updateHljsTheme() {
   if (!hljsTheme) return;
 
   if (document.documentElement.classList.contains("dark")) {
-    hljsTheme.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-dark.css";
+    hljsTheme.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-dark.css";
   } else {
-    hljsTheme.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-light.css";
+    hljsTheme.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-light.css";
   }
 }
 
@@ -16,3 +18,14 @@ document.querySelector(".theme").addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
   updateHljsTheme();
 });
+
+const showMoreBtn = document.querySelector(".show-more");
+
+if (showMoreBtn) {
+  showMoreBtn.addEventListener("click", () => {
+    document.querySelectorAll(".card[data-hidden]").forEach((card) => {
+      card.removeAttribute("data-hidden");
+    });
+    showMoreBtn.remove();
+  });
+}
