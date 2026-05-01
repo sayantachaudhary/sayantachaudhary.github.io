a0a0707da56ad366d6078a8cb27fe301c38f6eae
Sayanta Chaudhary <sayantachaudhary01@gmail.com>
2026-05-02 00:47:07 +0530

    defer js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,9 @@
-const hljsTheme = document.querySelector(".hljs-theme");
-
-function updateHljsTheme() {
-  if (!hljsTheme) return;
-
-  if (document.documentElement.classList.contains("dark")) {
-    hljsTheme.href =
-      "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-dark.css";
-  } else {
-    hljsTheme.href =
-      "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-light.css";
-  }
-}
-
-updateHljsTheme();
-
 document.querySelector(".theme").addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
   updateHljsTheme();
 });
 
 const showMoreBtn = document.querySelector(".show-more");
-
 if (showMoreBtn) {
   showMoreBtn.addEventListener("click", () => {
     document.querySelectorAll(".card[data-hidden]").forEach((card) => {
--- js/post.js
+++ js/post.js
@@ -1,25 +1,18 @@
-// Likes
-const SUPABASE_URL = "https://odwkgteyeycunqqrukkh.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kd2tndGV5ZXljdW5xcXJ1a2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzM1MTMsImV4cCI6MjA4OTk0OTUxM30.XvzWKgakd7S5kfCzJm0RC_80vWwVA2vkLZgzvYPHKMg";
+const hljsTheme = document.querySelector(".hljs-theme");
 
-async function getLikes(pid) {
-  const res = await fetch(`${SUPABASE_URL}/rest/v1/likes?post_id=eq.${pid}`, {
-    headers: { apikey: SUPABASE_KEY },
-  });
-  const data = await res.json();
-  return data[0]?.count ?? 0;
+function updateHljsTheme() {
+  if (!hljsTheme) return;
+
+  if (document.documentElement.classList.contains("dark")) {
+    hljsTheme.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-dark.css";
+  } else {
+    hljsTheme.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/atom-one-light.css";
+  }
 }
 
-async function addLike(pid) {
-  await fetch(`${SUPABASE_URL}/rest/v1/rpc/increment_like`, {
-    method: "POST",
-    headers: {
-      apikey: SUPABASE_KEY,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ post_id: pid }),
-  });
-}
+updateHljsTheme();
 
 // Lightbox effect
 document.querySelectorAll(".post img").forEach((img) => {
